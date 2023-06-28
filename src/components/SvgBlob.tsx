fe8a00329c076db2364c74eafda17f14e472a5ad
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-06-28 13:06:27 -0300

    demo section complete

--- src/components/SvgBlob.tsx
+++ src/components/SvgBlob.tsx
@@ -17,9 +17,9 @@ const Svg = styled.svg.attrs({
 })`
   fill: #e6f3ff;
   position: absolute;
-  left: -10%;
+  left: -12%;
   bottom: -100%;
-  z-index: -2;
+  z-index: -1;
 `;
 
 const Rect = styled.rect.attrs({
@@ -30,29 +30,3 @@ const Rect = styled.rect.attrs({
   rx: '80',
   transform: 'rotate(-45 -32.9326 582.239)',
 })``;
-
-
-// const Svg = styled.svg.attrs({
-//     xmlns: 'http://www.w3.org/2000/svg',
-//     viewBox: '0 0 1272 1272',
-// })`
-//   fill: #F2F9FF;
-//   width: 1272px;
-//   height: 1272px;
-//   position: absolute;
-//   left: -10%;
-//   bottom: 0%;
-//   transform: translateY(100px)
-// `;
-
-// const Path = styled.path.attrs({
-//     // d: 'M1524.45 723.55C1320.78 448.64 1099.04 29.5 906.98 62.32 733.88 91.9 602.27 447 436.26 735.34c-138.69 240.88-505.04 690.78-290.8 902.83 139.86 138.44 460.78 71.54 865.54 79.75 300.95 6.1 748.4 91.56 876.99-126.19 132.63-224.59-174.48-612.99-363.54-868.18Z',
-//     d: `
-//     M500,100 Q600,200 700,100
-//     L1700,1600 Q1500,1500 1300,1600
-//     L300,1600 Q600,1500 700,1600
-//     Z
-//   `,
-// })``;
-
-
--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -3,49 +3,91 @@ import { SvgBlob } from '../SvgBlob'
 import { styled } from 'styled-components'
 import Image from 'next/image'
 import comparativo from './img/comparativo.png'
+import rd from './img/selo_RD.png'
+import noCard from './img/no-card.webp'
+import rating from './img/rating.webp'
 
 export default function Demo() {
   return (
     <SectionDemo>
-        <SvgBlob/>
+      <SvgBlob />
 
-        <ImageContainer>
-          <Image src={comparativo} alt={'Comparativo'} width={700} height={651} loading='lazy'/>
-        </ImageContainer>
+      <ImageContainer>
+        <Image src={comparativo} alt={'Comparativo'} width={700} height={651} loading='lazy' />
+      </ImageContainer>
+
+      {/* Container Geral direita */}
+      <ContainerGeral>
+
+        {/* Container Texto */}
+        <ContainerText>
+          <TitleDemo>
+            Pronto para triplicar sua
+            <TitleStrong>
+              Geração de Leads?
+            </TitleStrong>
+          </TitleDemo>
+
+          <SubtitleDemo>
+            Criação e ativação em
+
+            <Strong>
+              4 minutos.
+            </Strong>
+
+          </SubtitleDemo>
+        </ContainerText>
+        {/*Fim Container Texto */}
 
 
-      <ContainerText>
+        <DivContainerGeral>
 
-      <TitleDemo>
-        Pronto para triplicar sua 
-        <TitleStrong>
-          Geração de Leads? 
-        </TitleStrong>
-      </TitleDemo>
+          {/* Container botão e image*/}
+          <ContainerButtonImg>
+            <Button>
+              Ver demonstração
+            </Button>
 
-      <SubtitleDemo>
-        Criação e ativação em 
-        
-        <SubtitleStrong>
-          4 minutos.
-        </SubtitleStrong>
+            <ImageContainer>
+              <Image src={rd} alt={'Top 10 Apps mais usados'} width={205} height={73} loading='lazy' />
+            </ImageContainer>
 
-      </SubtitleDemo>
+          </ContainerButtonImg>
+          {/* Fim Container botão e image*/}
 
-      </ContainerText>
+          <ContainerRating>
+            <CreditCard>
+              <Image src={noCard} alt={'Não é necessário Cartão de Crédito'} width={16} height={16} loading='lazy'/>
+              <ParagraphRating>
+                <Strong>Não</Strong> é necessário Cartão de Crédito |
+              </ParagraphRating>
+            </CreditCard>
+            
+            <Rating>
+              <Image src={rating} alt={'4.9/5 média de satisfação'} width={92} height={16} loading='lazy' />
+              <ParagraphRating>
+                <Strong>4.9</Strong>/5 média de satisfação
+              </ParagraphRating>
+            </Rating>
+          </ContainerRating>
+
+        </DivContainerGeral>
+      </ContainerGeral>
 
     </SectionDemo>
   )
 }
 
-const SectionDemo = styled.section `
+const SectionDemo = styled.section`
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1.2rem;
+  gap: 4rem;
   overflow: hidden;
+
+  z-index: 0;
 
   background-color: #f0f8ff;
 
@@ -54,44 +96,114 @@ const SectionDemo = styled.section `
   }
 `
 
-const ImageContainer = styled.div `
-  height: auto;
-  width: auto;
-  flex-basis: 30%;
-`
-const TitleDemo = styled.h2 `
-  font-size: 3rem;
-  font-weight: 500;
-`
-
-const ContainerText = styled.div `
+const ContainerGeral = styled.div`
   color: #2c3e50;
-  gap: 6px;
-  display: flex;
+  width: 35%;
+  gap: 1.2rem;
   text-align: left;
-  flex-basis: 30%;
+  display: flex;
   flex-direction: column;
   justify-content: start;
   align-items: start;
-
+`
+const ContainerText = styled.div`
   border-bottom: 1px solid #c8d4dd;
   padding-bottom: 1.3rem;
-`
+  gap: 12px;
 
-const  TitleStrong = styled.strong `
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+`
+const ImageContainer = styled.div`
+  height: auto;
+  width: auto;
+`
+const TitleDemo = styled.h2`
+  font-size: 3rem;
+  font-weight: 500;
+`
+const TitleStrong = styled.strong`
   font-weight: 700;
   display: flex;  
   flex-direction: column;
   align-items: flex-start;
 `
-
-const SubtitleDemo = styled.h3 `
+const SubtitleDemo = styled.h3`
   font-size: 1.1rem;
   font-weight: 500;
   display: flex;
   flex-direction: row;
   gap: .3ch;
 `
-const SubtitleStrong = styled.strong `
+const Strong = styled.strong`
   font-weight: 700;
+`
+const DivContainerGeral = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: start;
+  align-items: start;
+`
+const ContainerButtonImg = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: start;
+  align-items: center;
+`
+const ContainerRating = styled.div`
+  display: flex;
+  min-width: min-content;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: start;
+  align-items: center;
+`
+const CreditCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  justify-content: start;
+  align-items: center;
+`
+const Rating = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  justify-content: start;
+  align-items: center;
+`
+const ParagraphRating = styled.p `
+  font-size: 1rem;
+  font-weight: 600;
+  color: #2c3e50;
+`
+const Button = styled.button`
+  box-shadow: 0 20px 30px 0 rgba(12, 0, 46, .1);
+  background-color: #007dff;
+  display: inline-block;
+  padding: 18px 30px;
+
+  border-radius: 45px;
+  border: 1px solid #007dff;
+  transition: all .3s linear;
+
+  color: #fff;
+  font-size: 1.1rem; 
+  font-family: inherit;
+
+  cursor: pointer;
+
+  font-weight: 600;
+  text-transform: uppercase;
+
+  &:hover{
+    background-color: transparent;
+    outline: 1px solid #007dff;
+    color: #007dff;
+  }
 `
